a24b61adfc38e7a5cbf074872945cfae4f00eba3
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-13 16:26:15 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from "./navbar/NavBar.js"
+import Search from "./search/Search.js";
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+          <NavBar />
+        <div style={{padding: '10px'}}>
+          <Search />
+        </div>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
